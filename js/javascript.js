cbdc73f7c8e8e912bff30297918e1780e78ec034
Ashikur Rahman <smashikurrahman24@gmail.com>
2021-08-21 17:19:57 +0600

    update

--- js/javascript.js
+++ js/javascript.js
@@ -42,7 +42,7 @@ function memoryVariation(memoryPrice){
 
     if(extraStorageCost.innerText == 100) {
         const extraStorage = extraStorageCost.innerText;
-        totalPrice.innerText = bestPriceValue + parseInt(extraStorage) + parseInt(extraMemoryCost.innerText);
+        totalPrice.innerText = bestPriceValue + parseInt(extraStorageCost.innerText) + parseInt(extraMemoryCost.innerText);
 
         // Total
         const total = document.getElementById('total-amount');
@@ -82,6 +82,15 @@ function storageVariation(storagePrice) {
         const total = document.getElementById('total-amount');
         total.innerText = bestPriceValue + parseInt(extraMemory) + parseInt(extraMemoryCost.innerText);
     }
+     if(extraStorageCost.innerText == 100){
+        const extraMemory = extraMemoryCost.innerText;
+
+        totalPrice.innerText = bestPriceValue + parseInt(extraStorageCost.innerText);
+
+        // Total
+        const total = document.getElementById('total-amount');
+        total.innerText =bestPriceValue + parseInt(extraStorageCost.innerText);
+    }
 }
 
 // Delivery Cost
@@ -89,42 +98,62 @@ function deliveryCharge(charge) {
     const deliveryCharge = document.getElementById('delivery-cost');
     deliveryCharge.innerText = charge;
 
-    if(deliveryCharge.innerText == 20) {
+    if(deliveryCharge.innerText ==charge) {
         const bestPrice = document.getElementById('best-price');
         const bestPriceValue = parseInt(bestPrice.innerText);
         const totalPrice = document.getElementById('total-price');
 
         const extraStorageCost = document.getElementById('storage-cost');
         const extraMemoryCost = document.getElementById('memory-cost');
-    }
-    if(bestPrice.innerText == 1299) {
+    
+     if(bestPrice.innerText == 1299) {
         totalPrice.innerText = parseInt(bestPrice.innerText) + charge;
     }
-    if(extraMemoryCost.innerText == 180){
+     if(extraMemoryCost.innerText == 180){
         const extraMemory = extraMemoryCost.innerText;
         const memoryValue = bestPriceValue + parseInt(extraMemory);
         totalPrice.innerText = memoryValue + charge;
-        document.getElementById('total-amount') = memoryValue + charge;
+        document.getElementById('total-amount').innerText = memoryValue + charge;
 
     }
-    if(extraStorageCost.innerText == 100 || extraStorageCost.innerText == 180){
-        totalPrice.innerText = (bestPriceValue + parseInt(extraStorageCost.innerText)) + charge;
+     if(extraStorageCost.innerText == 100){
+        const extraMemory = extraMemoryCost.innerText;
+        const memoryValue = bestPriceValue + parseInt(extraMemory);
+        totalPrice.innerText =  memoryValue  + parseInt(extraStorageCost.innerText) + charge;
         const total = document.getElementById('total-amount');
         total.innerText = (bestPriceValue + parseInt(extraStorageCost.innerText)) + charge;
     }
-    if(bestPrice.innerText == 1299 && extraMemoryCost.innerText == 180 && extraStorageCost.innerText == 100) {
+     if(bestPrice.innerText == 1299 && extraMemoryCost.innerText == 180 && extraStorageCost.innerText == 100) {
+        totalPrice.innerText = parseInt(bestPrice.innerText) + parseInt(extraStorageCost.innerText) + parseInt(extraMemoryCost.innerText)+charge;
+
+        const total = document.getElementById('total-amount');
+        total.innerText = bestPriceValue + parseInt(extraStorageCost.innerText) + parseInt(extraMemoryCost.innerText) + charge;
+    }
+     if(bestPrice.innerText == 1299 && extraMemoryCost.innerText == 180 && extraStorageCost.innerText == 180) {
         totalPrice.innerText = (bestPriceValue + parseInt(extraStorageCost.innerText) + parseInt(extraMemoryCost.innerText)) + charge;
 
         const total = document.getElementById('total-amount');
         total.innerText = (bestPriceValue + parseInt(extraStorageCost.innerText) + parseInt(extraMemoryCost.innerText)) + charge;
     }
-    if(bestPrice.innerText == 1299 && extraMemoryCost.innerText == 180 && extraStorageCost.innerText == 180) {
-        totalPrice.innerText = (bestPriceValue + parseInt(extraStorageCost.innerText) + parseInt(extraMemoryCost.innerText)) + charge;
 
-        const total = document.getElementById('total-amount');
-        total.innerText = (bestPriceValue + parseInt(extraStorageCost.innerText) + parseInt(extraMemoryCost.innerText)) + charge;
-    }
-    else{
+     // Promo code matcher
+
+  /*   document.getElementById('apply-button').addEventListener('click', function(){
+        getInputValue();
+    })
+
+    function getInputValue() {
+
+        const codeMatcher = document.getElementById('promo-code');
+        if(codeMatcher.value == 'stevekaku') {
+            const totalPrice = document.getElementsById('total-price');
+            const totalAmount = document.getElementById('total-amount');
+            const totalValue = parseFloat(totalPrice.innerText);
+            totalAmount.innerText = totalValue * .2;
+        }
+    } */
+    
+   /*  else{
         const bestPrice = document.addEventListener('best-price');
         const bestPriceValue = parseInt(bestPrice.innerText);
         const totalPrice = document.getElementById('total-price');
@@ -133,31 +162,32 @@ function deliveryCharge(charge) {
         const extraMemoryCost = document.getElementById('memory-cost')
 
         if(bestPrice.innerText == 1299) {
-            totalPrice.innerText = parseInt(bestPrice.innerText) + charge;
+            totalPrice.innerText = parseInt(bestPrice.innerText);
         }
         if(extraMemoryCost.innerText == 180){
             const extraMemory = extraMemoryCost.innerText;
             const memoryValue = bestPriceValue + parseInt(extraMemory);
             totalPrice.innerText = memoryValue + charge;
-            document.getElementById('total-amount') = memoryValue + charge;
+            document.getElementById('total-amount') = memoryValue;
     
         }
         if(extraStorageCost.innerText == 100 || extraStorageCost.innerText == 180){
-            totalPrice.innerText = (bestPriceValue + parseInt(extraStorageCost.innerText)) + charge;
+            totalPrice.innerText = (bestPriceValue + parseInt(extraStorageCost.innerText));
             const total = document.getElementById('total-amount');
             total.innerText = (bestPriceValue + parseInt(extraStorageCost.innerText)) + charge;
         }
         if(bestPrice.innerText == 1299 && extraMemoryCost.innerText == 180 && extraStorageCost.innerText == 100) {
-            totalPrice.innerText = (bestPriceValue + parseInt(extraStorageCost.innerText) + parseInt(extraMemoryCost.innerText)) + charge;
+            totalPrice.innerText = (bestPriceValue + parseInt(extraStorageCost.innerText) + parseInt(extraMemoryCost.innerText));
     
             const total = document.getElementById('total-amount');
-            total.innerText = (bestPriceValue + parseInt(extraStorageCost.innerText) + parseInt(extraMemoryCost.innerText)) + charge;
+            total.innerText = (bestPriceValue + parseInt(extraStorageCost.innerText) + parseInt(extraMemoryCost.innerText));
         }
         if(bestPrice.innerText == 1299 && extraMemoryCost.innerText == 180 && extraStorageCost.innerText == 180) {
-            totalPrice.innerText = (bestPriceValue + parseInt(extraStorageCost.innerText) + parseInt(extraMemoryCost.innerText)) + charge;
+            totalPrice.innerText = (bestPriceValue + parseInt(extraStorageCost.innerText) + parseInt(extraMemoryCost.innerText));
     
             const total = document.getElementById('total-amount');
-            total.innerText = (bestPriceValue + parseInt(extraStorageCost.innerText) + parseInt(extraMemoryCost.innerText)) + charge;
+            total.innerText = (bestPriceValue + parseInt(extraStorageCost.innerText) + parseInt(extraMemoryCost.innerText));
         }
-    }
+    } */
+}
 }
